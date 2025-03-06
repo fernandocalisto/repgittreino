93b5256c862a7728a306bfcafab07e92084b97e1
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-06 14:18:11 -0300

    Início do Treino

---
+++ app.js
@@ -0,0 +1,7 @@
+let lista = [];
+let linguagens = ['Javascript','C','C++','Kotlin','Python'];
+linguagens.push('Java','Ruby','GoLang');
+let listaGenérica = ['José','Maria','Fernando'];
+console.log(listaGenérica[0]);
+console.log(listaGenérica[1]);
+console.log(listaGenérica[listaGenérica.length-1]);
